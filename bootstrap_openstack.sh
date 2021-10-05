afce579b195e0e5a03c7b7dd03105737165ebe96
Farhan Patwa <farhan.patwa@gmail.com>
2021-10-05 04:21:32 +0000

    Install Docker before bootstap-servers

--- bootstrap_openstack.sh
+++ bootstrap_openstack.sh
@@ -2,6 +2,16 @@
 
 set -euo pipefail
 
+#
+# Install Docker
+#
+apt-get update
+apt-get -y install apt-transport-https ca-certificates curl gnupg lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+apt-get -y install docker-ce docker-ce-cli containerd.io
+
 if [ $# -ge 1 ] && [ -n "$1" ]; then
   DOCKER_REGISTRY_PASSWORD=$1
 else
