391393d20aa029845f41f434cffd002f3c072c80
Farhan Patwa <farhan.patwa@gmail.com>
2021-10-05 04:26:44 +0000

    Add sudo for apt-get commands.

--- bootstrap_openstack.sh
+++ bootstrap_openstack.sh
@@ -5,12 +5,14 @@ set -euo pipefail
 #
 # Install Docker
 #
-apt-get update
-apt-get -y install apt-transport-https ca-certificates curl gnupg lsb-release
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt-get update
-apt-get -y install docker-ce docker-ce-cli containerd.io
+sudo apt-get update
+sudo apt-get -y install apt-transport-https ca-certificates curl gnupg lsb-release
+#
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+#
+sudo apt-get update
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
 
 if [ $# -ge 1 ] && [ -n "$1" ]; then
   DOCKER_REGISTRY_PASSWORD=$1
