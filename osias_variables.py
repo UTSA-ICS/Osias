3a437b7032212f1495eff7d3dd6701e5fad553c8
James Benson <james.o.benson@gmail.com>
2021-12-13 22:50:10 +0000

    update cirros to 0.5.2

--- osias_variables.py
+++ osias_variables.py
@@ -55,7 +55,7 @@ MAAS_VM_DISTRO = {
 # https://docs.openstack.org/placement/latest/placement-api-microversion-history.html
 # https://docs.openstack.org/releasenotes/tempest/unreleased.html
 REFSTACK_TEST_IMAGE = (
-    "https://download.cirros-cloud.net/0.5.1/cirros-0.5.1-x86_64-disk.img"
+    "https://download.cirros-cloud.net/0.5.2/cirros-0.5.2-x86_64-disk.img"
 )
 REFSTACK_TEST_VERSION = {
     "ussuri": "2020.06",
