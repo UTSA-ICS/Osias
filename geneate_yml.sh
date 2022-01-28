d033f0fd2c15084f9f1d91290f96e18e150eda11
Farhan Patwa <farhan.patwa@gmail.com>
2022-01-28 15:09:10 -0600

    Fixed Misc items

--- geneate_yml.sh
+++ geneate_yml.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-current_release=$(python3 -c "import json;import os;release=json.loads(os.getenv('VM_PROFILE_CURRENT_RELEASE'));print(release['OPENSTACK_RELEASE'])")
-previous_release=$(python3 -c "import json;import os;release=json.loads(os.getenv('VM_PROFILE_PREVIOUS_RELEASE'));print(release['OPENSTACK_RELEASE'])")
 
-deploy_pipeline="
-
+if [[ $DEPLOY_MULTIPLE_RELEASES == "true" ]];
+then
+    current_release=$(python3 -c "import json;import os;release=json.loads(os.getenv('VM_PROFILE_CURRENT_RELEASE'));print(release['OPENSTACK_RELEASE'])")
+    previous_release=$(python3 -c "import json;import os;release=json.loads(os.getenv('VM_PROFILE_PREVIOUS_RELEASE'));print(release['OPENSTACK_RELEASE'])")
+    deploy_pipeline="
 stages:
     - deploy
 
-$current_release:
+deploy:$current_release:
     stage: deploy
     variables:
         VM_PROFILE: \"\$VM_PROFILE_CURRENT_RELEASE\"
@@ -18,7 +19,7 @@ $current_release:
         include: deploy-pipeline.yml
         strategy: depend
 
-$previous_release:
+deploy:$previous_release:
     stage: deploy
     variables:
         VM_PROFILE: \"\$VM_PROFILE_PREVIOUS_RELEASE\"
@@ -27,6 +28,23 @@ $previous_release:
     trigger:
         include: deploy-pipeline.yml
         strategy: depend
+"
+else
+    release=$(python3 -c "import json;import os;release=json.loads(os.getenv('VM_PROFILE'));print(release['OPENSTACK_RELEASE'])")
+    deploy_pipeline="
+stages:
+    - deploy
+
+deploy:$release:
+    stage: deploy
+    variables:
+        VM_PROFILE: \"\$VM_PROFILE\"
+        REPROVISION_SERVERS: \"\$REPROVISION_SERVERS\"
+        DOCKER_REGISTRY_PASSWORD: \"\$DOCKER_REGISTRY_PASSWORD\"
+    trigger:
+        include: deploy-pipeline.yml
+        strategy: depend
+fi
 "
 
 echo "$deploy_pipeline"
