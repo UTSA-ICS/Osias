bf334870b4e7b69c3119e7d18caca5f70c11a0fc
Farhan Patwa <farhan.patwa@gmail.com>
2022-01-28 13:44:35 -0600

    Try dynamic child pipeline

---
+++ geneate_yml.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+current_release=$(python3 -c "import json;import os;release=json.loads(os.getenv('VM_PROFILE_CURRENT_RELEASE'));print(release['OPENSTACK_RELEASE'])")
+previous_release=$(python3 -c "import json;import os;release=json.loads(os.getenv('VM_PROFILE_PREVIOUS_RELEASE'));print(release['OPENSTACK_RELEASE'])")
+
+deploy_pipeline="
+
+stages:
+    - deploy
+
+$current_release:
+    variables:
+        VM_PROFILE: "\$VM_PROFILE_CURRENT_RELEASE"
+        REPROVISION_SERVERS: "\$REPROVISION_SERVERS"
+        DOCKER_REGISTRY_PASSWORD: "\$DOCKER_REGISTRY_PASSWORD"
+    trigger:
+        include: deploy-pipeline.yml
+        strategy: depend
+
+$previous_release:
+    variables:
+        VM_PROFILE: "\$VM_PROFILE_PREVIOUS_RELEASE"
+        REPROVISION_SERVERS: "\$REPROVISION_SERVERS"
+        DOCKER_REGISTRY_PASSWORD: "\$DOCKER_REGISTRY_PASSWORD"
+    trigger:
+        include: deploy-pipeline.yml
+        strategy: depend
+"
+
+echo "$deploy_pipeline"
