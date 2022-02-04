1b817dcb349652efbd796a0f50dcf0419900ff6f
Farhan Patwa <farhan.patwa@gmail.com>
2022-02-04 14:55:57 -0600

    Misc updates

--- generate_yml.sh
+++ generate_yml.sh
@@ -8,18 +8,18 @@ then
     sed -i "s/<RELEASE_NAME>/${current_release}/g" trigger-pipeline.yml
     sed -i "s/<PREVIOUS_RELEASE_NAME>/${previous_release}/g" trigger-pipeline.yml
 
-    cp deploy-pipeline.yml deploy-${current_release}.yml
-    sed -i "s/RELEASE_NAME/${current_release}/g" deploy-${current_release}.yml
+    cp deploy-pipeline.yml deploy-"${current_release}".yml
+    sed -i "s/RELEASE_NAME/${current_release}/g" deploy-"${current_release}".yml
 
-    cp deploy-pipeline.yml deploy-${previous_release}.yml
-    sed -i "s/RELEASE_NAME/${previous_release}/g" deploy-${previous_release}.yml
+    cp deploy-pipeline.yml deploy-"${previous_release}".yml
+    sed -i "s/RELEASE_NAME/${previous_release}/g" deploy-"${previous_release}".yml
 else
     release=$(python3 -c "import json;import os;release=json.loads(os.getenv('VM_PROFILE'));print(release['OPENSTACK_RELEASE'])")
 
     sed -i "s/<RELEASE_NAME>/${release}/g" trigger-pipeline.yml
 
-    cp deploy-pipeline.yml deploy-${release}.yml
-    sed -i "s/RELEASE_NAME/${release}/g" deploy-${release}.yml
+    cp deploy-pipeline.yml deploy-"${release}".yml
+    sed -i "s/RELEASE_NAME/${release}/g" deploy-"${release}".yml
 fi
 
 cat trigger-pipeline.yml
