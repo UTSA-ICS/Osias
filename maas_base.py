efe4cdd6fbc24a577c392cdf16e37b246a190d4c
James Benson <james.o.benson@gmail.com>
2022-03-02 21:21:51 +0000

    bugfix

--- maas_base.py
+++ maas_base.py
@@ -254,14 +254,11 @@ class MaasBase:
         servers = copy.deepcopy(server_list)
         while len(servers) > 0:
             if len(servers) == 1:
-                read_cmd = f"machine read {servers[0]}"
+                read_cmd = f"machine read {servers[0]} | jq '"
             else:
-                read_cmd = "machines read"
-            cmd = (
-                " | jq '.[] | {system_id:.system_id,status_name:.status_name,status_message:.status_message"
-                ",pool_name:.pool.name,ip_addresses:.ip_addresses}' --compact-output"
-            )
-            machine_info_list = self._run_maas_command(f"{read_cmd} {cmd}")
+                read_cmd = "machines read | jq '.[] | "
+            fields = "{system_id:.system_id,status_name:.status_name,status_message:.status_message,pool_name:.pool.name,ip_addresses:.ip_addresses}' --compact-output"
+            machine_info_list = self._run_maas_command(f"{read_cmd}{fields}")
             for server in servers[:]:
                 for machine in machine_info_list:
                     if str(server) in machine["system_id"]:
