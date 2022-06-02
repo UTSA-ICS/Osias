c7fe4457dc6be63a787f623cebeb5f1a161167ba
James Benson <james.o.benson@gmail.com>
2022-06-02 18:28:24 +0000

    Update bootstrap_podman.sh

--- bootstrap_podman.sh
+++ bootstrap_podman.sh
@@ -8,7 +8,7 @@ sudo apt-get update
 source /etc/os-release
 # Install podman
 echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/ /" | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
-curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/Release.key | sudo apt-key add -
+curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_"${VERSION_ID}"/Release.key | sudo apt-key add -
 
 # Update to fetch the package index for ceph added above
 sudo apt-get update
