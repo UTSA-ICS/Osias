425ec7096a2aad5367e7d32d726a217ab477cd06
James Benson <james.o.benson@gmail.com>
2022-08-01 22:45:06 +0000

    generate the ceph variable

--- generate_yml.sh
+++ generate_yml.sh
@@ -5,6 +5,18 @@ then
     current_release=$(python3 -c "import json;import os;release=json.loads(os.getenv('VM_PROFILE_CURRENT_RELEASE'));print(release['OPENSTACK_RELEASE'])")
     previous_release=$(python3 -c "import json;import os;release=json.loads(os.getenv('VM_PROFILE_PREVIOUS_RELEASE'));print(release['OPENSTACK_RELEASE'])")
 
+    if [ -z "$VM_PROFILE_CURRENT_RELEASE" ]
+        ceph_current_bool=$(python3 -c "import os,json; multi=json.loads(os.getenv('VM_PROFILE_CURRENT_RELEASE'));ceph=multi['CEPH'];print(ceph)")
+    fi
+    if [ -z "$VM_PROFILE" ]
+        ceph_current_bool=$(python3 -c "import os,json; multi=json.loads(os.getenv('VM_PROFILE'));ceph=multi['CEPH'];print(ceph)")
+    fi
+    if [ -z "$VM_PROFILE_PREVIOUS_RELEASE" ]
+        ceph_previous_bool=$(python3 -c "import os,json; multi=json.loads(os.getenv('VM_PROFILE_PREVIOUS_RELEASE'));ceph=multi['CEPH'];print(ceph)")
+    fi
+    
+    sed -i "s/<CEPH_CURRENT_BOOL>/${ceph_current_bool}/g" trigger-pipeline.yml
+    sed -i "s/<CEPH_PREVIOUS_BOOL>/${ceph_previous_bool}/g" trigger-pipeline.yml
     sed -i "s/<RELEASE_NAME>/${current_release}/g" trigger-pipeline.yml
     sed -i "s/<RELEASE_VM_PROFILE>/\$VM_PROFILE_CURRENT_RELEASE/g" trigger-pipeline.yml
     sed -i "s/<PREVIOUS_RELEASE_NAME>/${previous_release}/g" trigger-pipeline.yml
@@ -17,6 +29,14 @@ then
 else
     release=$(python3 -c "import json;import os;release=json.loads(os.getenv('VM_PROFILE'));print(release['OPENSTACK_RELEASE'])")
 
+    if [ -z "$VM_PROFILE_CURRENT_RELEASE" ]
+        ceph_current_bool=$(python3 -c "import os,json; multi=json.loads(os.getenv('VM_PROFILE_CURRENT_RELEASE'));ceph=multi['CEPH'];print(ceph)")
+    fi
+    if [ -z "$VM_PROFILE" ]
+        ceph_current_bool=$(python3 -c "import os,json; multi=json.loads(os.getenv('VM_PROFILE'));ceph=multi['CEPH'];print(ceph)")
+    fi
+    
+    sed -i "s/<CEPH_CURRENT_BOOL>/${ceph_current_bool}/g" trigger-pipeline.yml
     sed -i "s/<RELEASE_NAME>/${release}/g" trigger-pipeline.yml
     sed -i "s/<RELEASE_VM_PROFILE>/\$VM_PROFILE/g" trigger-pipeline.yml
 
