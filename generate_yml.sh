acf7afeaee0a93abec7bc92aee8568e25d53b613
Farhan Patwa <farhan.patwa@gmail.com>
2022-08-02 03:25:50 +0000

    Debugging why ceph pool is not being resolved.

--- generate_yml.sh
+++ generate_yml.sh
@@ -5,15 +5,8 @@ then
     current_release=$(python3 -c "import json;import os;release=json.loads(os.getenv('VM_PROFILE_CURRENT_RELEASE'));print(release['OPENSTACK_RELEASE'])")
     previous_release=$(python3 -c "import json;import os;release=json.loads(os.getenv('VM_PROFILE_PREVIOUS_RELEASE'));print(release['OPENSTACK_RELEASE'])")
 
-    if [ -z "$VM_PROFILE_CURRENT_RELEASE" ]; then
-        ceph_current_bool=$(python3 -c "import os,json; multi=json.loads(os.getenv('VM_PROFILE_CURRENT_RELEASE'));ceph=multi['CEPH'];print(ceph.lower())")
-    fi
-    if [ -z "$VM_PROFILE" ]; then
-        ceph_current_bool=$(python3 -c "import os,json; multi=json.loads(os.getenv('VM_PROFILE'));ceph=multi['CEPH'];print(ceph.lower())")
-    fi
-    if [ -z "$VM_PROFILE_PREVIOUS_RELEASE" ]; then
-        ceph_previous_bool=$(python3 -c "import os,json; multi=json.loads(os.getenv('VM_PROFILE_PREVIOUS_RELEASE'));ceph=multi['CEPH'];print(ceph.lower())")
-    fi
+    ceph_current_bool=$(python3 -c "import os,json; multi=json.loads(os.getenv('VM_PROFILE_CURRENT_RELEASE'));ceph=multi['CEPH'];print(ceph.lower())")
+    ceph_previous_bool=$(python3 -c "import os,json; multi=json.loads(os.getenv('VM_PROFILE_PREVIOUS_RELEASE'));ceph=multi['CEPH'];print(ceph.lower())")
     
     sed -i "s/<CEPH_CURRENT_BOOL>/${ceph_current_bool}/g" trigger-pipeline.yml
     sed -i "s/<CEPH_PREVIOUS_BOOL>/${ceph_previous_bool}/g" trigger-pipeline.yml
@@ -29,12 +22,7 @@ then
 else
     release=$(python3 -c "import json;import os;release=json.loads(os.getenv('VM_PROFILE'));print(release['OPENSTACK_RELEASE'])")
 
-    if [ -z "$VM_PROFILE_CURRENT_RELEASE" ]; then
-        ceph_current_bool=$(python3 -c "import os,json; multi=json.loads(os.getenv('VM_PROFILE_CURRENT_RELEASE'));ceph=multi['CEPH'];print(ceph.lower())")
-    fi
-    if [ -z "$VM_PROFILE" ]; then
-        ceph_current_bool=$(python3 -c "import os,json; multi=json.loads(os.getenv('VM_PROFILE'));ceph=multi['CEPH'];print(ceph.lower())")
-    fi
+    ceph_current_bool=$(python3 -c "import os,json; multi=json.loads(os.getenv('VM_PROFILE'));ceph=multi['CEPH'];print(ceph.lower())")
     
     sed -i "s/<CEPH_CURRENT_BOOL>/${ceph_current_bool}/g" trigger-pipeline.yml
     sed -i "s/<RELEASE_NAME>/${release}/g" trigger-pipeline.yml
