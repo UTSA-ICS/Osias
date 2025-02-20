f506912b0ed64f679bf973c83e3149295e3913e8
James Benson <JamesOBenson@users.noreply.github.com>
2025-02-20 13:55:20 -0600

    test bridge creation

--- bootstrap_networking.sh
+++ bootstrap_networking.sh
@@ -4,22 +4,34 @@ set -euxo pipefail
 
 # Add br0 bridge if it does not exist
 br0_exists() { ip addr show br0 &>/dev/null; }
-if ! br0_exists; then
+if ! br0_exists; thenif ! br0_exists; then
+    sudo apt install -y bridge-utils
     netplan_file="50-cloud-init.yaml" # "00-installer-config.yaml"
     # Get existing MAC address
-    mac_address=$(grep macaddress /etc/netplan/${netplan_file} | awk '{print $2}')
+    mac_address=$(sudo grep macaddress /etc/netplan/${netplan_file} | awk '{print $2}' | head -n 1)
     # Get the interface name (remove the ":" from the name)
-    interface_name=$(grep -A 1 ethernets /etc/netplan/${netplan_file} | grep -v ethernets | awk '{print $1}')
+    interface_name=$(sudo grep -A 1 ethernets /etc/netplan/${netplan_file} | grep -v ethernets | awk '{print $1}')
     interface_name=${interface_name%:}
-    cat /etc/netplan/${netplan_file}
+    eth0_ip=$(ip address show dev eth0 | grep 'inet ' | awk '{print $2}')
+    eth0_ip0=$(ip route | grep "eth0 proto kernel scope link src" | awk '{print $1}')
+    temp=$(echo $eth0_ip  | awk -F'/' '{print $1}')
+    eth0_router=$(ip route | grep $temp | grep default | awk '{print $3}')
+    sudo cat /etc/netplan/${netplan_file}
     cat /etc/hosts
     cat /etc/resolv.conf
     # Copy to work with a temp file
     cp /etc/netplan/${netplan_file} /tmp/${netplan_file}
-    # Now modify the temp file to add the bridge information
-    echo -ne "    bridges:
+        # Now modify the temp file to add the bridge information
+    sudo brctl addbr br0
+    sudo ip link set dev br0 up
+    echo -ne "  bridges:
       br0:
-          dhcp4: true
+          addresses:
+              - $eth0_ip
+          routes:
+            -   metric: 0
+                to: $eth0_ip0
+                via: $eth0_router
           interfaces:
               - $interface_name
           macaddress: $mac_address
