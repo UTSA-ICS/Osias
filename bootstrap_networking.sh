cca049010832a0971e061e61b7fce31f4d04ea14
James Benson <JamesOBenson@users.noreply.github.com>
2025-02-20 13:58:57 -0600

    fix shellscript

--- bootstrap_networking.sh
+++ bootstrap_networking.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 
 # Add br0 bridge if it does not exist
 br0_exists() { ip addr show br0 &>/dev/null; }
-if ! br0_exists; thenif ! br0_exists; then
+if ! br0_exists; then
     sudo apt install -y bridge-utils
     netplan_file="50-cloud-init.yaml" # "00-installer-config.yaml"
     # Get existing MAC address
@@ -14,8 +14,8 @@ if ! br0_exists; thenif ! br0_exists; then
     interface_name=${interface_name%:}
     eth0_ip=$(ip address show dev eth0 | grep 'inet ' | awk '{print $2}')
     eth0_ip0=$(ip route | grep "eth0 proto kernel scope link src" | awk '{print $1}')
-    temp=$(echo $eth0_ip  | awk -F'/' '{print $1}')
-    eth0_router=$(ip route | grep $temp | grep default | awk '{print $3}')
+    temp=$(echo "$eth0_ip"  | awk -F'/' '{print $1}')
+    eth0_router=$(ip route | grep "$temp" | grep default | awk '{print $3}')
     sudo cat /etc/netplan/${netplan_file}
     cat /etc/hosts
     cat /etc/resolv.conf
