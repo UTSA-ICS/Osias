de9d695a873eda1b23bc0b4f2123d24aa5106c5c
James Benson <james.o.benson@gmail.com>
2025-09-05 12:11:28 -0500

    small patches

--- utils.py
+++ utils.py
@@ -250,14 +250,6 @@ def create_multinode(input_dictionary, optional_variables):
     # Safely parse the YAML string into a dictionary
     optional_variables = yaml.safe_load(optional_variables)
     multinode["variables"].update(optional_variables)
-    # multinode["variables"] = {}
-    # optional_variables = dict(
-    #     [
-    #         (x.split(":")[0].strip(), x.split(":")[1].strip("' "))
-    #         for x in optional_variables.strip("{}").split(",")
-    #     ]
-    # )
-    # multinode["variables"].update(optional_variables)
     return multinode
 
 
